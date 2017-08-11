6079adcd62524ba49794635da6ef476ab41766d6
Daniel <danielalegria1217@gmail.com>
2017-08-11 13:33:00 -0700

    routine commit

--- dist/bundle.js
+++ dist/bundle.js
@@ -187,6 +187,47 @@ angular.module("blog")
 		};
 	}]);
 
+angular.module("blog").controller("navCtrl", ["$scope", "authService", "$state", function($scope, authService, $state) {
+  $scope.logout = function() {
+    authService.logout().then(function(response) {
+      $state.go('login');
+    });
+  };
+}]);
+
+angular.module('blog').directive('navDir', function() {
+  return {
+    restrict: 'EA',
+    templateUrl: './app/directives/nav/navTmpl.html',
+    controller: 'navCtrl',
+    link: function(scope, element, attribute) {
+
+      // nav dropdown toggle:
+      $('nav ul li > a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        // if there is more than one toggle button, this prevents all toggles from opening:
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+
+      // close toggle when you click outside of it:
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+      });
+
+      // toggle hamburger
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+
+      $('#nav-toggle').click(function() {
+        $('nav ul').toggle();
+      });
+
+    }
+  };
+});
+
 angular.module("blog").controller("adminCtrl", ["$scope", "user", "authService", function($scope, user, authService) {
 
 		$scope.user = user;
@@ -295,44 +336,3 @@ angular.module("blog").controller("loginCtrl", ["$scope", "authService", "$state
     });
   };
 }]);
-
-angular.module("blog").controller("navCtrl", ["$scope", "authService", "$state", function($scope, authService, $state) {
-  $scope.logout = function() {
-    authService.logout().then(function(response) {
-      $state.go('login');
-    });
-  };
-}]);
-
-angular.module('blog').directive('navDir', function() {
-  return {
-    restrict: 'EA',
-    templateUrl: './app/directives/nav/navTmpl.html',
-    controller: 'navCtrl',
-    link: function(scope, element, attribute) {
-
-      // nav dropdown toggle:
-      $('nav ul li > a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        // if there is more than one toggle button, this prevents all toggles from opening:
-        $('.nav-dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
-      });
-
-      // close toggle when you click outside of it:
-      $('html').click(function() {
-        $('.nav-dropdown').hide();
-      });
-
-      // toggle hamburger
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-
-      $('#nav-toggle').click(function() {
-        $('nav ul').toggle();
-      });
-
-    }
-  };
-});
